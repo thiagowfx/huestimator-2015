6a4a0b078cf2686ace7f7ee97626db8bc9bb6f4a
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-06-10 20:22:46 -0300

    add hello world

---
+++ src/main/scala/hello.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object Hello {
+
+  def main(args: Array[String]) {
+
+    // init spark context
+    val conf = new SparkConf().setAppName("huestimator")
+    val sc = new SparkContext(conf)
+
+    println("Hello World")
+
+    // terminate spark context
+    sc.stop()
+  }
+}
+
