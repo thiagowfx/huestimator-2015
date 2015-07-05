0bcf4e708831525239f1d0c1e89b14e141a879c2
Bernardo Amorim <contato@bamorim.com>
2015-07-05 20:23:59 -0300

    object Classifier and example in hello separating train and evaluation data

--- src/main/scala/br/ufrj/huestimator/hello.scala
+++ src/main/scala/br/ufrj/huestimator/hello.scala
@@ -4,10 +4,8 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.regression.LinearRegressionModel
-import org.apache.spark.mllib.regression.LinearRegressionWithSGD
-import org.apache.spark.mllib.linalg.Vectors
 
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -31,32 +29,28 @@ object Hello {
     val sc = new SparkContext(conf)
 
     // Load and parse the data
-    val data = sc.textFile("data/lpsa.data")
+    val data = sc.textFile("data/wdbc.data")
     
-    val parsedData = data.map {
-      line =>
-      val parts = line.split(',')
-      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
-    }.cache()
+    val parsedData = Classifier.parseData(data).cache()
 
-    // Building the model
-    val numIterations = 100
-    val model = LinearRegressionWithSGD.train(parsedData, numIterations)
+    // Split data into training (70%) and test (30%).
+    val splits = parsedData.randomSplit(Array(0.7, 0.3), seed = 11L)
+    val training = splits(0).cache()
+    val test = splits(1)
 
-    // Evaluate model on training examples and compute training error
-    val valuesAndPreds = parsedData.map {
-      point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
+    val model = Classifier.train(training)
+
+    // Compute raw scores on the test set.
+    val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+      val prediction = model.predict(features)
+      (prediction, label)
     }
-    val MSE = valuesAndPreds.map{case(v, p) => math.pow((v - p), 2)}.mean()
-    println("training Mean Squared Error = " + MSE)
 
-    // Save and load model
-    // model.save(sc, "myModelPath")
-    // val sameModel = LinearRegressionModel.load(sc, "myModelPath")
+    // Get evaluation metrics.
+    val metrics = new MulticlassMetrics(predictionAndLabels)
+    val precision = metrics.precision
+    println("Precision = " + precision)
 
-    // terminate spark context
     sc.stop()
   }
 }
