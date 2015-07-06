ae746689e551def727f5bfb443e07bbe4aeb1c12
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-07-06 20:28:21 -0300

    update main.js to handle 30 parameters and return them to the server a csv string

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -3,41 +3,72 @@ $(document).ready(function() {
 });
 
 function populateResults(data) {
-	if(data.error !== undefined) {
-		$("#results").html('<p style="color: red; font-size: 1.3em;"><span style="font-weight: bold; text-decoration: underline;">Erro</span>: ' + data.error + "</p>");
-		return;
+	if (data.error !== undefined) {
+		$("#results")
+				.html(
+						'<p style="color: red; font-size: 1.3em;"><span style="font-weight: bold; text-decoration: underline;">Erro</span>: '
+								+ data.error + "</p>");
 	}
-	
-	// TODO: popular os resultados
-	$("#results").html('TODO: popular os resultados. Data:<br />' + JSON.stringify(data));
+	else {
+		$("#results").html(data.response);
+	}
 }
 
 function submit_form() {
 	$("#results").html('');
 
-	var formData = {
-		'radius': $("#radius").val(),
-		'texture': $("#texture").val(),
-		'perimeter': $("#perimeter").val(),
-		'area': $("#area").val(),
-		'suavization': $("#suavization").val(),
-		'compacity': $("#compacity").val(),
-		'concavity': $("#concavity").val(),
-		'concave_points': $("#concave_points").val(),
-		'simmetry': $("#simmetry").val(),
-		'fractal_dimension': $("#fractal_dimension").val()
-	};
+	var dataArr = [ 
+	                'radius_m', 
+	                'texture_m',
+	                'perimeter_m',
+	                'area_m',
+	                'suavization_m',
+	                'compacity_m',
+	                'concavity_m',
+	                'concave_points_m',
+	                'simmetry_m',
+	                'fractal_dimension_m',
+	                'radius_e', 
+	                'texture_e',
+	                'perimeter_e',
+	                'area_e',
+	                'suavization_e',
+	                'compacity_e',
+	                'concavity_e',
+	                'concave_points_e',
+	                'simmetry_e',
+	                'fractal_dimension_e',
+	                'radius_w', 
+	                'texture_w',
+	                'perimeter_w',
+	                'area_w',
+	                'suavization_w',
+	                'compacity_w',
+	                'concavity_w',
+	                'concave_points_w',
+	                'simmetry_w',
+	                'fractal_dimension_w',
+	                ]
+
+	var dataStr = '';
+	for (var i = 0; i < dataArr.length; i++) {
+		if (i > 0) {
+			dataStr += ',';
+		}
+		dataStr += $("#" + dataArr[i]).val();
+	}
 
 	$.ajax({
-		url: '/predict',
-		method: 'POST',
-		data: formData,
-		success: function (data, status, jqxhr) {
-			console.log('-- success --')
+		url : '/predict',
+		method : 'POST',
+		data : {
+			'data' : dataStr
+		},
+		success : function(data, status, jqxhr) {
 			$(window).scrollTo(0, 800);
 			populateResults(data);
 		},
-		error: function() {
+		error : function() {
 			window.alert("Ocorreu um erro: o servidor não pode ser acessado!");
 		}
 	});
